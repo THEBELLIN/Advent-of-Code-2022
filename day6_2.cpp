74c160d947420d096d1277470fe5bfc1d31f53a7
THEBELLIN <36982778+THEBELLIN@users.noreply.github.com>
2022-12-09 11:27:37 +0100

    Add files via upload

---
+++ day6_2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	deque<char> buffer;
+	char r;
+	int ans=14;
+	for(int i=0; i<14; i++)
+	{
+		cin>>r;
+		buffer.push_back(r);
+	}
+	while(1)
+	{
+		cin>>r;
+		bool duplicate = false;
+		for(int i=0; i<buffer.size() && !duplicate; i++)
+		{
+			for(int j=i+1; j<buffer.size(); j++)
+			{
+				if(buffer[i]==buffer[j])
+				{
+					duplicate = true;
+					break;
+				}
+			}
+		}
+		if(duplicate)
+		{
+			buffer.push_back(r);
+			buffer.pop_front();
+			ans++;
+		}
+		else
+			break;
+	}
+	cout<<ans;
+}
